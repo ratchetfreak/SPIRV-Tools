bff70224a83d1dc823c53b53e981109ccd6505d9
ratchetfreak <ratchet.freak@gmail.com>
2016-09-09 00:13:59 +0200

    initial setup for var2reg

---
+++ source/opt/var2reg_pass.cpp
@@ -0,0 +1,107 @@
+// Copyright (c) 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "var2reg_pass.h"
+
+#include "def_use_manager.h"
+#include "basic_block.h"
+#include "function.h"
+#include "instruction.h"
+
+#include "spirv/1.1/spirv.h"
+
+#include <unordered_set>
+
+namespace spvtools {
+namespace opt {
+
+bool Mem2RegPass::Process(ir::Module* module) {
+  analysis::DefUseManager def_use(module);
+  
+
+  bool modified = false;
+  
+  for(auto& func : *module){
+    std::unordered_set<ir::Instruction*> working_list;
+    ir::BasicBlock* startBlock = &*func.begin();
+    
+    for(auto &instruction: *startBlock){
+      if(instruction.opcode() == SpvOpVariable){
+        working_list.insert(&instruction);
+        
+        repeat_from_scratch:
+
+        bool loaded = false;
+        bool gepped = false;
+        bool passedToFunc = false;
+        bool storedCount = 0;
+        
+        analysis::UseList* uselist = def_use.GetUses(instruction.result_id());
+
+        for(auto& use: *uselist){
+          switch(use.inst->opcode()){
+            case SpvOpAccessChain:
+            case SpvOpInBoundsAccessChain:{
+                gepped = true;
+            }break;
+            case SpvOpCopyMemory: {
+                if (use.operand_index == 0) storedCount++;
+                if (use.operand_index == 1) loaded = true;
+            }break;
+            case SpvOpCopyObject: {
+                def_use.ReplaceAllUsesWith(use.inst->result_id(), instruction.result_id());
+                def_use.KillInst(use.inst);
+
+                //iterator we are using just changed, have to break out
+                goto repeat_from_scratch;
+            }break;
+            case SpvOpPhi:{
+                //not legal but bail anyway
+                passedToFunc = true;
+            }break;
+            case SpvOpFunctionCall: 
+            case SpvOpExtInst:{
+                //passed to function/extension function not much to do...
+                passedToFunc = true;
+            }break;
+
+            default: {
+                //some unkown use ... bail out
+                passedToFunc = true;
+            }break;
+          }
+        }
+
+        if (!loaded) {
+            for (auto& use : *uselist) {
+                def_use.KillInst(use.inst);
+            }
+            working_list.erase(working_list.find(&instruction));
+            def_use.KillInst(&instruction);
+        }
+        
+        //can't remove variable when used as parameter.
+        if(passedToFunc)break;
+      }
+    }
+    
+    
+    
+  }
+  
+  return modified;
+}
+
+}  // namespace opt
+}  // namespace spvtools
